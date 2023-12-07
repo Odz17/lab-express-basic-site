95c03b7bddbf65872862b509e9f6bdeb3c8e3dc9
Jose Ordaz <145297953+Odz17@users.noreply.github.com>
2023-12-07 23:45:15 +0100

    lab done

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'home.html'));
+});
+
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'about.html'));
+});
+
+app.get('/works', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'works.html'));
+});
+
+app.get('/gallery', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'gallery.html'));
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${3000}`);
+});
